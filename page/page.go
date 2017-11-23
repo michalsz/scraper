8cea335a0874196483f25a50b50c76649f7d9206
Michal Szymanski <michal.szymanski@xing.com>
2017-11-23 09:15:23 +0100

    Parse page by getTokens

--- page/page.go
+++ page/page.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type Content interface {
@@ -17,8 +19,10 @@ type Page struct {
 	Body string ""
 }
 
-func (page Page) PageBody() string {
-	return page.Body
+type Node struct {
+	Type  string
+	Token html.Token
+	Doc   *html.Tokenizer
 }
 
 func (page *Page) ReadPage() {
@@ -35,6 +39,53 @@ func (page *Page) ReadPage() {
 	page.Body = string(body)
 }
 
+func (page *Page) GetTokensFromBody(wantedTokens []string) (c chan Node) {
+	c = make(chan Node)
+
+	go func() {
+		defer close(c)
+
+		// https://play.golang.org/p/0MRSefJ_-E
+		r := strings.NewReader(page.Body)
+		z := html.NewTokenizer(r)
+
+		for {
+			tt := z.Next()
+
+			switch {
+			case tt == html.ErrorToken:
+				// End of the document, we're done
+				return
+			case tt == html.StartTagToken:
+				token := z.Token()
+
+				for _, name := range wantedTokens {
+					if token.Data == name {
+						c <- Node{token.Data, token, z}
+					}
+					continue
+				}
+			}
+		}
+	}()
+
+	return c
+}
+
 func IsHttps(page Page) bool {
 	return strings.Contains(page.Url, "https")
+}
+
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	// "bare" return will return the variables (ok, href) as defined in
+	// the function definition
+	return
 }
--- scraper.go
+++ scraper.go
@@ -3,17 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
-
+	"strings"
 	"github.com/michalsz/scraper/page"
-	//"strconv"
+	"golang.org/x/net/html"
 )
 
 func main() {
 	url := os.Args[1]
-	fmt.Println(url)
+	fmt.Println("Page to parse: ", url)
 	wwwPage := page.Page{url, ""}
-	for i := 0; i < 5; i++ {
-		wwwPage.ReadPage()
-		fmt.Println(wwwPage.PageBody())
+	wwwPage.ReadPage()
+	// fmt.Println(wwwPage.Body)
+	var chanNode chan page.Node
+	var title string
+	var a []string
+	wantedTokens := []string{
+		"a", "title",
 	}
+
+	chanNode = wwwPage.GetTokensFromBody(wantedTokens)
+
+	for node := range chanNode {
+		// fmt.Println(node.Token)
+		if node.Type == "title" {
+			fmt.Println(node.Token)
+			tt := node.Doc.Next()
+
+			if tt == html.TextToken {
+				next := node.Doc.Token()
+				title = strings.TrimSpace(next.Data)
+			}
+		}
+
+		 if node.Type == "a" {
+		 	fmt.Println(node.Token)
+		 	a = append(a, node.Type)
+		 }
+
+	}
+
+	fmt.Println("title", title)
+	fmt.Println("a", a)
 }
